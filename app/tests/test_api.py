93c79b8abb3913dd65688474ef89ff1f26694d18
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2024-02-29 17:49:25 -0500

    Update .gitignore and test_api.py, add example.env file
    - Remove *.env from .gitignore
    - Add test_api.py with new test case for processing URLs
    - Update MINIO_ROOT_USER and MINIO_ROOT_PASSWORD in docker-compose.yaml
    - Add example.env file with MINIO_ROOT_USER and MINIO_ROOT_PASSWORD

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,13 +1,15 @@
+
+
 import httpx
 import pytest
 
 @pytest.mark.asyncio
 async def test_process_urls():
+    urls = ["https://sanity.cdaprod.dev", "https://blog.min.io/author/david-cannan"]
     async with httpx.AsyncClient() as client:
         response = await client.post(
-            "http://python-app:8000/process-urls/",
-            json={"urls": ["https://sanity.cdaprod.dev"]}
+            "http://localhost:8000/process-urls/",
+            json={"urls": urls}
         )
-        assert response.status_code == 200
-        assert "URLs processed and stored in MinIO and Weaviate successfully" in response.text
-
+        assert response.status_code == 200 
+        assert "Hydrated services with URLs, processed and stored in MinIO and Weaviate successfully" in response.text
